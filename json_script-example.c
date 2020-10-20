79e63a6d034cbc3344d169bc9ef050ca73edc5d6
招文桃 <zhao@wen-tao.com>
2020-10-20 13:55:23 +0800

    ubox Demo works

---
+++ json_script-example.c
@@ -0,0 +1,23 @@
+#include <libubox/uloop.h>
+#include <stdio.h>
+struct uloop_timeout timeout;	//创建uloop_timeout全局变量
+
+int frequency = 5; //每隔5秒超时一次
+
+static void timeout_cb(struct uloop_timeout *t)
+{
+    //do your work
+    puts("What is going on?\n");
+    uloop_timeout_set(t, frequency * 1000);//设置下次的超时时间
+}
+
+int main()
+{
+    uloop_init();			//使用库初始化
+
+    timeout.cb = timeout_cb;
+
+    uloop_timeout_set(&timeout, frequency * 1000);//设置下次的超时时间
+
+    uloop_run();
+}
